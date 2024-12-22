f0b13893a860c47c90520720416414245da0b424
farid <valkanid12@gmail.com>
2024-12-22 21:47:26 +0700

    clerk login

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,26 @@
+"use client";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import { useUser } from "@clerk/nextjs";
+
 function Header() {
-  return <div>Header</div>;
+  const { user } = useUser();
+
+  return (
+    <div className="flex justify-between p-5">
+      {user && <h1 className="text-2xl">{user?.firstName}</h1>}
+
+      {/* breadcrumbs */}
+
+      <div>
+        <SignedOut>
+          <SignInButton></SignInButton>
+        </SignedOut>
+
+        <SignedIn>
+          <UserButton></UserButton>
+        </SignedIn>
+      </div>
+    </div>
+  );
 }
 export default Header;
---
+++ components/Sidebar.tsx
@@ -0,0 +1,31 @@
+import { MenuIcon } from "lucide-react";
+import NewDocumentButton from "./NewDocumentButton";
+import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+
+function Sidebar() {
+  const menuOption = (
+    <>
+      <NewDocumentButton></NewDocumentButton>
+    </>
+  );
+  return (
+    <div className="p-2 md:p-5 relative bg-gray-200">
+      <div className="md:hidden">
+        <Sheet>
+          <SheetTrigger>
+            <MenuIcon size={40} className="p-2 hover:opacity-30 rounded-lg"></MenuIcon>
+          </SheetTrigger>
+          <SheetContent side="left" className="w-[400px] sm:w-[540px]">
+            <SheetHeader>
+              <SheetTitle>Menu</SheetTitle>
+              {menuOption}
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
+
+      <div className="hidden md:inline">{menuOption}</div>
+    </div>
+  );
+}
+export default Sidebar;
