fcaffbdb0dc2f36767e18f565a8ffbe8eddb960d
farid <valkanid12@gmail.com>
2024-12-28 21:24:18 +0700

    create new document firebase

--- components/NewDocumentButton.tsx
+++ components/NewDocumentButton.tsx
@@ -1,6 +1,23 @@
+"use client";
+import { useTransition } from "react";
 import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
+import { createNewDocument } from "@/actions/actions";
 
 function NewDocumentButton() {
-  return <Button>New Document</Button>;
+  const [isPending, startTranstition] = useTransition();
+  const router = useRouter();
+
+  function handleCreateNewDocumnetButton() {
+    startTranstition(async () => {
+      const { docId } = await createNewDocument();
+      router.push(`/doc/${docId}`);
+    });
+  }
+  return (
+    <Button onClick={handleCreateNewDocumnetButton} disabled={isPending}>
+      {isPending ? "Creating..." : "New Document"}
+    </Button>
+  );
 }
 export default NewDocumentButton;
